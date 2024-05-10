9e60f4ee2902c009b4d19928166dfd12878cee35
dssq <danielssq89@gmail.com>
2024-05-10 09:28:02 +0800

    reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,138 +1,9 @@
 import React, { useCallback, useState, useRef } from 'react'
 import './App.css'
 import jsQR from 'jsqr'
-
-type KnownElement = {
-  description: string,
-  interpreter?: ((value: string) => string)
-}
-
-type InterpretationContext = {
-  name: string,
-  knownElements: Record<string, KnownElement>
-}
-
-const EMVCoMPMContext: InterpretationContext = {
-  name: 'EMVCo Merchant Presented QR Code',
-  knownElements: {
-    '00': {
-      description: 'Payload format indicator'
-    },
-    '01': {
-      description: 'Point of initiation'
-    },
-    '26': {
-      description: 'Merchant account information'
-    },
-    '52': {
-      description: 'Merchant category code'
-    },
-    '53': {
-      description: 'Transaction currency'
-    },
-    '58': {
-      description: 'Country code'
-    },
-    '59': {
-      description: 'Merchant name'
-    },
-    '60': {
-      description: 'Merchant city'
-    },
-    '63': {
-      description: 'CRC'
-    },
-  }
-}
-
-
-const UNKNOWN: unique symbol = Symbol()
-
-type ParsedElement = {
-  elementID: string,
-  length: number,
-  rawValue: string,
-  interpretation: string | ParseResult | null,
-  description: string | typeof UNKNOWN,
-}
-type ParseResult = {
-  context: InterpretationContext,
-  elements: Array<ParsedElement | ParseError>
-}
-
-class ParseError extends Error { }
-class InvalidLengthError extends ParseError {
-  lengthRequested: number
-  lengthAvailable: number
-  constructor(lengthRequested: number, lengthAvailable: number) {
-    super(`A length of ${lengthRequested} was requested, but only ${lengthAvailable} chars are left in the payload`)
-    this.lengthRequested = lengthRequested
-    this.lengthAvailable = lengthAvailable
-  }
-}
-class InvalidElementError extends ParseError {
-  element: string
-  constructor(element: string) {
-    super(`An invalid element+length indicator of ${element} was encountered`)
-    this.element = element
-  }
-}
-
-const parseData = (context: InterpretationContext, data: string): ParseResult => {
-  let index = 0
-  const elements: Array<ParseError | ParsedElement> = []
-
-  while (index < data.length) {
-    const elementAndLength = data.substring(index, index + 4)
-
-    index += 4
-
-    if (elementAndLength.length < 4) {
-      elements.push(new InvalidElementError(elementAndLength))
-      break;
-    }
-
-    const elementID = elementAndLength.substring(0, 2)
-    const lengthString = elementAndLength.substring(2, 4)
-    if (!lengthString.match(/[0-9]{2}/)) {
-      elements.push(new InvalidElementError(elementAndLength))
-      break;
-    }
-
-    const length = parseInt(lengthString)
-    const value = data.substring(index, index + length)
-
-    index += length
-
-    if (value.length < length) {
-      elements.push(new InvalidLengthError(length, value.length))
-      break;
-    }
-
-    if (elementID in context.knownElements) {
-      elements.push({
-        elementID,
-        length,
-        description: context.knownElements[elementID].description,
-        rawValue: value,
-        interpretation: context.knownElements[elementID].interpreter?.(value) || null,
-      })
-    } else {
-      elements.push({
-        elementID,
-        length,
-        description: UNKNOWN,
-        rawValue: value,
-        interpretation: null,
-      })
-    }
-  }
-
-  return {
-    context,
-    elements,
-  }
-}
+import { UNKNOWN, ParseError, parseData, EMVCoMPMContext } from './EMVCoMPMContext'
+import type { ParseResult } from './EMVCoMPMContext'
+import { createPortal } from 'react-dom'
 
 function renderDescription(s: string | typeof UNKNOWN): React.ReactNode {
   if (typeof s === 'string') {
@@ -142,24 +13,46 @@ function renderDescription(s: string | typeof UNKNOWN): React.ReactNode {
   }
 }
 
-function EMVCoInterpretation(props: { data: string }) {
-  const parsedData = parseData(EMVCoMPMContext, props.data)
+function renderInterpretation(s: string | ParseResult | null) {
+  if (typeof s === 'string') {
+    return s
+  } else if (s === null) {
+    return null
+  } else {
+    return renderParseResult(s)
+  }
+}
 
+function EMVCoInterpretation(props: { data: string }) {
+  const parseResult = parseData(EMVCoMPMContext, props.data)
+
+  return renderParseResult(parseResult)
+}
+
+function renderParseResult(parseResult: ParseResult) {
   return <table>
     <thead>
-      <tr><th colSpan={3}>{parsedData.context.name}</th></tr>
+      <tr><th colSpan={3}>{parseResult.context.name}</th></tr>
     </thead>
-    {parsedData.elements.map((elem, index) => {
+    {parseResult.elements.map((elem, index) => {
       if (elem instanceof ParseError) {
         return (<tr key={index}><td colSpan={3}>{elem.message}</td></tr>)
       } else {
-        return (<tr key={index}><td>{elem.elementID}</td>
-          <td>{renderDescription(elem.description)}</td>
-          <td>
-            <RawValue value={elem.rawValue}></RawValue>
-
-            {typeof elem.interpretation === 'string' && elem.interpretation}
-          </td></tr>)
+        return (<>
+          <tr key={index} className="group-start">
+            <td>{elem.elementID}</td>
+            <td>{renderDescription(elem.description)}</td>
+            <td>
+              <RawValue value={elem.rawValue}></RawValue>
+            </td>
+          </tr>
+          {elem.interpretation && <tr>
+            <td></td>
+            <td colSpan={2}>
+              {renderInterpretation(elem.interpretation)}
+            </td>
+          </tr>}
+        </>)
       }
     })}
   </table>
@@ -203,6 +96,15 @@ function App() {
   const [isCapturing, setIsCapturing] = useState(false);
   const [data, setData] = useState<string | null>('propertyvaluescanbespecifiedasasinglekeywordchosenfromthelist')
 
+  const stopCapture = useCallback(() => {
+    if (video.current) {
+      video.current.srcObject = null
+      video.current.pause()
+      video.current = null
+    }
+    setIsCapturing(false)
+  }, [])
+
   const tick = useCallback(() => {
     const ve = video.current
 
@@ -237,7 +139,7 @@ function App() {
       }
     }
     requestAnimationFrame(tick);
-  }, [])
+  }, [stopCapture])
 
   const captureQR = useCallback(() => {
     if (!video.current) {
@@ -258,22 +160,26 @@ function App() {
     setIsCapturing(true)
   }, [tick])
 
-  const stopCapture = useCallback(() => {
-    if (video.current) {
-      video.current.srcObject = null
-      video.current.pause()
-      video.current = null
-    }
-    setIsCapturing(false)
-  }, [])
-
   return (
     <>
       {!isCapturing && <button onClick={captureQR}>Capture QR</button>}
       {isCapturing && <button onClick={stopCapture}>Stop Capture</button>}
-      <canvas ref={canvasRef} width="500" height="500" style={{ height: '100px', width: '100px', display: isCapturing ? 'block' : 'none' }}></canvas>
+      <PopupSpace style={{ display: isCapturing ? undefined : 'none', textAlign: 'center' }}>
+        <button onClick={stopCapture}>Stop Capture</button>
+        <canvas ref={canvasRef} width="500" height="500" style={{ height: 'auto', width: '100%', display: isCapturing ? 'block' : 'none' }}></canvas>
+      </PopupSpace>
       <RenderData data={data} />
     </>
+  )
+}
+
+
+function PopupSpace(props: { children: Array<React.ReactNode>, style: Record<string, string | number | null | undefined> }) {
+  return createPortal(
+    <div className="canvas-portal" style={props.style}>
+      {props.children}
+    </div>,
+    document.body
   )
 }
 
